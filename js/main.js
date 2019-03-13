f8509c74793b4cae99b6455541b190ad9df1e4a7
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2019-03-13 13:56:26 +0100

    website by ss imageless

--- js/main.js
+++ js/main.js
@@ -1,67 +1,47 @@
-$(function () {
-    if($(window).width() >= 1200){
-        $.scrollify({
-            section: ".main-section",
-            interstitialSection: '.footer',
-            scrollbars: false,
-            before: function (i, section) {
-                var ref = section[i].attr("data-section-name");
-                $(".pagination .active").removeClass("active");
-                $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-            },
-            afterRender: function () {
-                var pagination = "<ul class=\"pagination\">";
-                var activeClass = "";
-                $(".main-section").each(function (i) {
-                    activeClass = "";
-                    if (i === 0) {
-                        activeClass = "active";
-                    }
-                    pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(
-                            this).attr("data-section-name") +
-                        "\"><span class=\"hover-text\">" + $(this).attr(
-                            "data-section-name").charAt(0).toUpperCase() + $(this).attr(
-                            "data-section-name").slice(1) + "</span></a></li>";
-                });
-                pagination += "</ul>";
-                $("#main").append(pagination);
-                $(".pagination a").on("click", $.scrollify.move);
-            }
-        });
-    }
-});
+$(document).ready(function() {
 
-$(window).on('resize', function() {
-    if($(window).width() >= 1200){
-        $.scrollify({
-            section: ".main-section",
-            interstitialSection: '.footer',
-            scrollbars: false,
-            before: function (i, section) {
-                var ref = section[i].attr("data-section-name");
-                $(".pagination .active").removeClass("active");
-                $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-            },
-            afterRender: function () {
-                var pagination = "<ul class=\"pagination\">";
-                var activeClass = "";
-                $(".main-section").each(function (i) {
-                    activeClass = "";
-                    if (i === 0) {
-                        activeClass = "active";
-                    }
-                    pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(
-                            this).attr("data-section-name") +
-                        "\"><span class=\"hover-text\">" + $(this).attr(
-                            "data-section-name").charAt(0).toUpperCase() + $(this).attr(
-                            "data-section-name").slice(1) + "</span></a></li>";
-                });
-                pagination += "</ul>";
-                $("#main").append(pagination);
-                $(".pagination a").on("click", $.scrollify.move);
-            }
-        });
-    } else {
-        $.scrollify.destroy();
-    }
+	AOS.init();
+
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top - 80
+			}, 1000);
+		}
+	});
+
+	$(window).resize(function() {
+		var $window = $(window);
+		distance = $('body').offset().top + 20;
+		$window.scroll(function () {
+			if ($window.scrollTop() >= distance) {
+				$('header').addClass('mini');
+			} else {
+				$('header').removeClass('mini');
+			}
+		});
+	}).resize();
+
+	$('#toggler').on('click', function() {
+			$(this).toggleClass('opened');
+			$('#menu').slideToggle(300);
+	});
+
+	$('#menu a').on('click', function() {
+		if($(window).width() < 991) {
+			$('#toggler').toggleClass('opened');
+			$('#menu').slideToggle(300);
+		}
+	});
+
+	$('.tooltip-name').on('mouseenter', function(){
+		$(this).next('.tooltip-desc').slideToggle(200);
+	});
+
+	$('.tooltip-name').on('mouseleave', function(){
+		$(this).next('.tooltip-desc').slideToggle(200);
+	});
+
 });
